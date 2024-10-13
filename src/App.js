7990652b59fb70aa8287f984216573865d0ffd20
ackwolver335 <abhaych335@gmail.com>
2024-10-13 08:38:07 +0530

    App JS data Updated

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ const App = () =>{
         <Route exact path = "/newsapp" element = {<News setProgress={setProgress} apiKey={apiKey} key = "general" pageSize={pageSize} country="us" category="general"/>} />
 
         {/* Defining further subcategories here for different categories of news */}
-        <Route exact path = "/science" element = {<News setProgress={setProgress} apiKey={apiKey} key = "science" pageSize={pageSize} country="us" category="science"/>} />
-        <Route exact path = "/sports" element = {<News setProgress={setProgress} apiKey={apiKey} key = "sports" pageSize={pageSize} country="us" category="sports"/>} />
-        <Route exact path = "/business" element = {<News setProgress={setProgress} apiKey={apiKey} key = "business" pageSize={pageSize} country="us" category="business"/>} />
-        <Route exact path = "/health" element = {<News setProgress={setProgress} apiKey={apiKey} key = "health" pageSize={pageSize} country="us" category="health"/>} />
+        <Route path = "/science" element = {<News setProgress={setProgress} apiKey={apiKey} key = "science" pageSize={pageSize} country="us" category="science"/>} />
+        <Route path = "/sports" element = {<News setProgress={setProgress} apiKey={apiKey} key = "sports" pageSize={pageSize} country="us" category="sports"/>} />
+        <Route path = "/business" element = {<News setProgress={setProgress} apiKey={apiKey} key = "business" pageSize={pageSize} country="us" category="business"/>} />
+        <Route path = "/health" element = {<News setProgress={setProgress} apiKey={apiKey} key = "health" pageSize={pageSize} country="us" category="health"/>} />
 
         </Routes>
     </div>
